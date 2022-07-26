dcf8b21451f2121fceec4af46fb4d3ef9d94ad9d
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-07-26 06:39:07 -0300

    Iniciando projeto api  de mapeamento de luas

---
+++ src/test/java/com/api/moons/MoonsMappedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.moons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonsMappedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
