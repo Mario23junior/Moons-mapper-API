fdf9a9e51fcd8297364b61eabab06e705688f315
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-07-27 03:36:28 -0300

    Tratando e customizando retorno de error

---
+++ src/main/java/com/api/moons/handlerErros/CustonErroRetornoHandler.java
@@ -0,0 +1,28 @@
+package com.api.moons.handlerErros;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import com.api.moons.exceptions.ReturnObjectFailedExceptionsMessage;
+import com.api.moons.modelError.modelError;
+
+@RestControllerAdvice
+public class CustonErroRetornoHandler {
+
+	@ExceptionHandler(ReturnObjectFailedExceptionsMessage.class)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public modelError returnErroCustom(ReturnObjectFailedExceptionsMessage  ex, WebRequest request) {
+		modelError error = new modelError(
+				HttpStatus.NO_CONTENT.value(),
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+		
+				return error;	
+	}
+}
