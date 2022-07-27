598d44d00bff8c88736aeb879fe1a5a39c98186c
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-07-27 03:16:04 -0300

    Criando repository

---
+++ src/main/java/com/api/moons/repository/MoonsRepository.java
@@ -0,0 +1,11 @@
+package com.api.moons.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.moons.model.Moons;
+
+@Repository
+public interface MoonsRepository extends JpaRepository<Moons, Long>{
+
+}
