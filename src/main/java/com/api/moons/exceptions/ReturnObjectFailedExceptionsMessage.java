f0502008d3aa2bea96e809ea9d60cfc8c4440590
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-07-27 03:27:37 -0300

    Criando excecao customizada de retorno

---
+++ src/main/java/com/api/moons/exceptions/ReturnObjectFailedExceptionsMessage.java
@@ -0,0 +1,11 @@
+package com.api.moons.exceptions;
+
+public class ReturnObjectFailedExceptionsMessage extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ReturnObjectFailedExceptionsMessage(String message) {
+		super(message);
+	}
+
+}
